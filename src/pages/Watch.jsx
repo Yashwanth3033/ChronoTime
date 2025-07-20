c9f24d5d7d8ba324c0c6fd3aa18cc2ea4b84f9d2
Nico An <nidhish1233@gmail.com>
2025-07-20 23:36:13 +0530

    Made watch page responsive

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -20,21 +20,21 @@ function Watch() {
     if (!watch) return <div className={"flex flex-row item-center justify-center py-10"}><Spinner /></div>
     const {watchDescription, watchName, watchImage, watchPrice, watchBrand} = watch;
     return (
-        <div className="flex flex-col sm:flex-row py-3 gap-x-7">
+        <div className="flex flex-col lg:flex-row py-14 lg:py-6 gap-x-7 px-6">
             {/*product img*/}
-            <div className="basis-1/2 overflow-hidden rounded-4xl ring-3 ring-[#B87333]">
-                    <img src={watchImage} alt={watchName} className="w-full h-full hover:scale-110 transition-all cursor-pointer" />
+            <div className="basis-1/2 overflow-hidden rounded-4xl ring-3 ring-[#B87333] h-56 lg:h-full self-center">
+                    <img src={watchImage} alt={watchName} className="w-full lg:h-full object-cover h-96 hover:scale-110 transition-all cursor-pointer" />
             </div>
             {/*product details and buy*/}
-            <div className="basis-full px-6 py-3 my-20 gap-y-3 flex flex-col ">
+            <div className="basis-full px-6 py-3 my-20 gap-y-3 flex flex-col">
                 <h1 className="font-bold text-3xl tracking-wider font-serif">{watchName}</h1>
                 <p className="text-2xl font-bold tracking-widest bg-gradient-to-r from-yellow-400 via-lime-500
                 to-green-500 text-transparent bg-clip-text inline-block cursor-pointer">
                     {watchBrand}
                 </p>
-                <p className="text-gray-400 w-1/2">{watchDescription}</p>
+                <p className="text-gray-400 w-auto xl:w-10/12">{watchDescription}</p>
                 <h3 className="text-lg font-semibold">₹ {watchPrice}</h3>
-                <button className="px-6 py-3 text-xl rounded w-fit font-semibold tracking-tighter cursor-pointer bg-yellow-500 hover:bg-yellow-600 transition-all duration-200 hover:scale-105">Buy Now</button>
+                <button className="self-center md:self-auto w-full px-6 py-3 text-xl rounded md:w-fit font-semibold tracking-tighter cursor-pointer bg-yellow-500 hover:bg-yellow-600 transition-all duration-200 hover:scale-105">Buy Now</button>
             </div>
         </div>
     );
